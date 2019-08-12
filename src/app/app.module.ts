09906a493b610aad6f9d09fb53e058ba8865cb8c
CJ Werder <cj@CJs-MacBook-Pro.attlocal.net>
2019-08-11 21:31:10 -0400

    Enable basic routing in address bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LibraryComponent } from './library/library.component';
+import { ShopComponent } from './shop/shop.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: 'library', component: LibraryComponent},
+  { path: 'library/:id', component: LibraryComponent },
+  { path: 'login',  component: LoginComponent },
+  {
+    path: 'shop', component: ShopComponent
+  },
+  { path: '',
+    redirectTo: '/library',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LibraryComponent,
+    ShopComponent,
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/shop/shop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.less']
+})
+export class ShopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
